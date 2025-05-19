2e75317a27fac910dbad2e13e16d854ca6caec07
= <ajonasmorais@gmail.com>
2025-05-19 19:19:58 -0300

    commit inicial

---
+++ src/main/java/br/com/lincadinho/API_RESTful_lincadinho/ApiResTfulLincadinhoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lincadinho.API_RESTful_lincadinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiResTfulLincadinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiResTfulLincadinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lincadinho/API_RESTful_lincadinho/ApiResTfulLincadinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lincadinho.API_RESTful_lincadinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiResTfulLincadinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
